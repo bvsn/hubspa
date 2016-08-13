e473a6905fbd55bf3ba4f3e37c25fd402085e8f8
Andrew Bovsunovskiy <an.bvsn@gmail.com>
2016-08-13 19:30:34 +0300

    Fix: texts.

--- src/cljs/site/component.cljs
+++ src/cljs/site/component.cljs
@@ -32,23 +32,23 @@
     [:p (str "Software developer with 6 years of experience in web developing, "
              "creating projects from scratch, team management, high load systems.")]
 
-    [:p (str "I enjoy working in a small companies that create useful things. "
-             "My goal is to create software that will help solve/optimize people tasks. "
-             "Today I or assist such companies or itself creates such :)")]])
+    [:p (str "I enjoy working in small companies that create useful things. "
+             "My goal is to create software that will help to solve/optimize people's "
+             "tasks. Today I either assist such companies or create them by myself :)")]])
 
 (defn cv []
   [:div
     [:p (str "Like any other experienced developer I have extensive knowledge in "
              "different technologies and programming languages. In my training "
-             "and development I try to follow the principle: 2 years for one language. "
-             "Currently I am freely with such technologies: Python, JavaScript, "
-             "Clojure, ClojureScript, Erlang.")]
+             "and self-development I try to follow the principle: 2 years for "
+             "learning one language. Currently I am freely with such technologies: "
+             "Python, JavaScript, Clojure, ClojureScript, Erlang.")]
 
-    [:p (str "Yep of course, I said that 2 years for one language, but wrote a 4 "
-             "different languages. It's all overtimes :) During the last 3 years, "
-             "from time-to-time I'm doing my pet projects with the Erlang. "
-             "I really like him, but I only once used it in my work. I hope that "
-             "I will have the opportunity to work with him in any future project.")]
+    [:p (str "Yep, I said that 2 years for one language, but had written 4 "
+             "different languages. It's all overtimes :) During the last 3 years "
+             "from time to time I do my pet projects with the Erlang. "
+             "I really like it, but I have used it only once before in my work. I hope "
+             "I will have the opportunity to work with it in any future project.")]
 
     [:p.h-ta-c "..."]
   ])
