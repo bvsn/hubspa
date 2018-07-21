19cf637609adaa513acfe49da2b22d5df5849c22
Andrii Bovsunovskiy <an.bvsn@gmail.com>
2018-07-21 23:31:50 +0300

    Fix: styles

--- src/bvsn/components/cv.cljs
+++ src/bvsn/components/cv.cljs
@@ -2,10 +2,12 @@
 
 (defn component []
   [:div
-    [:p (str "Like any other experienced software engineer, I have extensive knowledge of different "
-             "technologies and programming languages. I try to follow the principle: each problem "
-             "should be solved with the specific technology and approach. Currently, I am freely "
-             "with such technologies: Python, JavaScript, Clojure, ClojureScript.")]
+    [:div.b-wrapper
+      [:section.b-container
+        [:p (str "Like any other experienced software engineer, I have extensive knowledge of different "
+                 "technologies and programming languages. I try to follow the principle: each problem "
+                 "should be solved with the specific technology and approach. Currently, I am freely "
+                 "with such technologies: Python, JavaScript, Clojure, ClojureScript.")]
 
-    [:p.h-ta-c "..."]
-  ])
+        [:p.h-ta-c "..."]
+      ]]])
